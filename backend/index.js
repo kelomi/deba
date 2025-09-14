538e858f8f93a662b6c8ae4ea313901eb40d8462
kelomi <elvisomi44@gmail.com>
2025-09-14 13:14:07 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware - FIXED CORS CONFIGURATION
 app.use(cors({
-  origin: 'http://localhost:5173', // Your frontend URL
+  origin: 'http://52.90.249.200:5173', // Your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
